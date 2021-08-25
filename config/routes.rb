1bf83c43ec2807a627f2ef3d79fd881f376319ee
Sam Needham <src.needham@gmail.com>
2021-08-25 12:45:29 +0100

    Updates root index to lists

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 require 'sidekiq/web'
 
 Rails.application.routes.draw do
-  resources :cards
-  resources :lists
   get '/privacy', to: 'home#privacy'
   get '/terms', to: 'home#terms'
 authenticate :user, lambda { |u| u.admin? } do
@@ -29,6 +27,10 @@ end
   resources :notifications, only: [:index]
   resources :announcements, only: [:index]
   devise_for :users, controllers: { omniauth_callbacks: "users/omniauth_callbacks" }
-  root to: 'home#index'
+
+  resources :lists
+  resources :cards
+  
+  root to: 'lists#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
