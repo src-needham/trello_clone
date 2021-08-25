f9fd0bb22b47badc8ca1a38b0cda295d8c56dadc
Sam Needham <src.needham@gmail.com>
2021-08-25 12:26:35 +0100

    Generates Card model

---
+++ app/views/cards/_card.json.jbuilder
@@ -0,0 +1,2 @@
+json.extract! card, :id, :list, :name, :postition, :created_at, :updated_at
+json.url card_url(card, format: :json)
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 require 'sidekiq/web'
 
 Rails.application.routes.draw do
+  resources :cards
   resources :lists
   get '/privacy', to: 'home#privacy'
   get '/terms', to: 'home#terms'
