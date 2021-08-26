f78b899a7a6a331d7e0a774231728b98da2fa3fe
Sam Needham <src.needham@gmail.com>
2021-08-26 17:31:14 +0100

    Alows new cards to be added and submitted

--- app/javascript/app.vue
+++ app/javascript/app.vue
@@ -1,11 +1,15 @@
 <template>
-  <div id="app">
-    <div v-for="list in original_lists" class="col-3" v-bind:key="list in original_lists">
+  <div id="app" class="row">
+    <div v-for="(list, index) in lists" class="col-3" v-bind:key="list in original_lists">
       <h6>{{ list.name }}</h6>
       <hr />
 
-      <div v-for="(card, index) in list.cards" class="card card-body" v-bind:key="(card, index) in list.cards">
+      <div v-for="(card, index) in list.cards" class="card card-body mb-3" v-bind:key="(card, index) in list.cards">
         {{ card.name }}
+      </div>
+      <div class="card card-body">
+        <textarea v-model="messages[list.id]" class="form-control"></textarea>
+        <button v-on:click="submtMessages(list.id)" class="btn btn-secondary">Add</button>
       </div>
     </div>
   </div>
@@ -14,7 +18,31 @@
 <script>
 export default {
   props: ["original_lists"],
+  data: function() {
+    return {
+      messages: {},
+      lists: this.original_lists
+    }
+  },
+  methods: {
+    submitMessages: function(list_id) {
+      var data = new FormData
+      data.append("card[list_id]", list_id)
+      data.append("card[name]", this.messages[list_id])
 
+      Rails.ajax({
+        url: "/cards",
+        type: "POST",
+        data: data,
+        dataType: "json",
+        success: (data) => {
+          const index = this.lists.findIndex(item => item.id == list_id),
+          this.lists[index].cards.push(data),
+          this.messages[list_id] = undefined
+        }
+      })
+    }
+  }
 }
 </script>
 
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,20 +2,42 @@
 // present in this directory. You're encouraged to place your actual application logic in
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
+require("@rails/ujs")
+require("turbolinks").start()
+require("channels")
+
+import "bootstrap"
+
+Rails.start()
+
+document.addEventListener("turbolinks:load", () => {
+  $('.tooltip').tooltip()
+  $('[rel="tooltip"]').tooltip()
+  $('[data-toggle="tooltip"]').tooltip()
+  $('[data-toggle="popover"]').popover()
+  $('[data-toggle="dropdown"]').dropdown()
+  $('.toast').toast({ autohide: false })
+  $('#toast').toast('show')
+})
 
 import Vue from 'vue/dist/vue.esm'
+import Vuex from 'vuex'
 import App from '../app.vue'
+import TurbolinksAdapter from 'vue-turbolinks'
+
+Vue.use(Vuex)
+Vue.use(TurbolinksAdapter)
 
 document.addEventListener("turbolinks:load", function() {
   var element = document.querySelector("#boards")
 
   if (element != undefined) {
+    window.store.state.lists = JSON.parse(element.dataset.lists)
+
     const app = new Vue({
       el: element,
-      data: {
-        lists: JSON.parse(element.dataset.lists)
-      },
-      template: "<App :original_lists='lists' />",
+      store: window.store,
+      template: "<App />",
       components: { App }
     })
   }
